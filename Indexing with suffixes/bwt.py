e88a99735788d47f44cbb93ce3c41536b4e307c2
Frank <frank840629@gmail.com>
2022-01-22 18:46:22 +0800

    add BWT from SA

--- Indexing with suffixes/bwt.py
+++ Indexing with suffixes/bwt.py
@@ -2,19 +2,24 @@
 
 def rotations(t):
     # Return list of rotations of input string t
-    tt = t * 2
+    tt = t * 2 # in order to make index reasonable
     return [ tt[i:i+len(t)] for i in range(len(t)) ]
 
 def bwm(t):
-    # Return lexicographically sorted list of t’s rotations
+    # Return lexicographically sorted list of t’s rotations (Burrows-Wheeler matrix)
     return sorted(rotations(t))
 
 def bwtViaBwm(t):
-    # Given T, returns BWT(T) by way of the BWM
+    # Given T, returns BWT(T) by way of the BWM 
+    # i.e., take the last column of bwm
+    # map (function, iterable)
+    # lambda parameter_list: expression
     return ''.join(map(lambda x: x[-1], bwm(t)))
 
 def suffixArray(s):
+    """ Given T return suffix array SA(T). """
     satups = sorted([(s[i:], i) for i in range(len(s))])
+    # Extract and return just the offsets
     return map(lambda x: x[1], satups)
 
 def bwtViaSa(t):
@@ -33,6 +38,9 @@ if __name__ == '__main__':
   print(bwm('abaaba$'))
   print('\n'.join(bwm('abaaba$')))
 
-  print("BWT is : " + bwtViaBwm('abaaba$')) # we can see the result equals the last column of the matrix above
+  print("BWT of \"abaaba\" is : " + bwtViaBwm('abaaba$')) # we can see the result equals the last column of the matrix above
 
   print(bwtViaBwm('abaaba$'), bwtViaSa('abaaba$')) # same result
+
+  print("BWT of \"banana\" is : " + bwtViaSa("banana$"))
+
